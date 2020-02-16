afbd977710e63f0e926aef021bf017a1ba3489d3
Josh Duff <me@JoshDuff.com>
2020-02-16 15:00:14 -0600

    Notes

--- README.md
+++ README.md
@@ -2,5 +2,16 @@
 
 Because every other spreadsheet-like form on the web sucks.
 
+## Status
+
+This is just a hobby project written in anger, it's not a real thing yet.
+
 ## In progress
 
+- dropdowns
+- resizable columns
+	- emitting an event on resize so that you can store them in user preferences
+- change detection, with an event fired when an item changes
+- jank
+	- field background color when checkboxes have focus
+	- zeros after the decimal point on number inputs in Firefox
--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,9 @@
-<!--
-- styling based on column type (e.g. font-variant-numeric)
-- the ability to use custom format functions per-column, with defaults based on column type
-- checkboxes, dropdowns, text/numeric inputs
-- resizable columns, emitting an event on resize so that you can store them in user preferences
-- change detection, with an event fired when an item changes
--->
 <script>
 	import ListInput from './ListInput.svelte'
-	
+
 	const numberFormatter = precision => number => number === null ? '' : number.toFixed(precision)
-	
-	
+
+
 	const columns = [
 		{ name: 'Taxable', property: 'taxable', type: 'boolean' },
 		{ name: 'Description', property: 'description', type: 'string' },
@@ -19,7 +12,7 @@
 		{ name: 'Tax', property: 'tax', type: 'number', step: '.01', formatter: numberFormatter(2), writeable: false },
 		{ name: 'Total', property: 'total', type: 'number', step: '.01', formatter: numberFormatter(2), writeable: false },
 	]
-	
+
 	const items = [
 		{ quantity: 2, description: 'Pants', taxable: true, price: 20, tax: 1.5, total: 21.5 },
 		{ quantity: 1, description: 'Sock', taxable: true, price: 3.99, tax: .30, total: 4.29 },
